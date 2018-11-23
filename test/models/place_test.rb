b86cec506e196f4cb0c9684ba0fd5fade2b47e0b
Simon Soriano <simonsp@uber.com>
2018-11-24 00:09:40 +0100

    Install annotate and annotate models

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id              :integer          not null, primary key
+#  country         :string
+#  city            :string
+#  price_per_night :decimal(, )
+#  bedrooms        :integer
+#  bathrooms       :integer
+#  guests_capacity :integer
+#  owner_id        :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
